50d2b832b54000941283c219ba436d1e84587cfb
olivier felce <olivierfelce@hotmail.com>
2021-01-10 22:36:55 +0100

    DEV: CREATE statements added

--- sql_queries.py
+++ sql_queries.py
@@ -1,44 +1,140 @@
 # DROP TABLES
 
-songplay_table_drop = "DROP table IF EXISTS songplay"
-user_table_drop = "DROP table IF EXISTS user"
-song_table_drop = "DROP table IF EXISTS song"
-artist_table_drop = "DROP table IF EXISTS artist"
+songplay_table_drop = "DROP table IF EXISTS songplays"
+user_table_drop = "DROP table IF EXISTS users"
+song_table_drop = "DROP table IF EXISTS songs"
+artist_table_drop = "DROP table IF EXISTS artists"
 time_table_drop = "DROP table IF EXISTS time"
 
 # CREATE TABLES
 
 songplay_table_create = ("""
+    CREATE TABLE IF NOT EXISTS songplays 
+    (
+        songplay_id int PRIMARY KEY,
+        start_time timestamp,
+        user_id varchar,
+        level varchar, 
+        song_id varchar,
+        artist_id varchar,
+        session_id varchar,
+        location varchar,
+        user_agent varchar
+    );
 """)
 
 user_table_create = ("""
+    CREATE TABLE IF NOT EXISTS users 
+    (
+        user_id varchar PRIMARY KEY,
+        first_name varchar, 
+        last_name varchar, 
+        gender varchar, 
+        level varchar
+    );
 """)
 
 song_table_create = ("""
+    CREATE TABLE IF NOT EXISTS songs
+    (
+        song_id varchar PRIMARY KEY , 
+        title varchar, 
+        artist_id varchar, 
+        year int, 
+        duration decimal   
+    );
 """)
 
 artist_table_create = ("""
+    CREATE TABLE IF NOT EXISTS artists
+    (
+        artist_id varchar PRIMARY KEY, 
+        name varchar, 
+        location varchar, 
+        latitude decimal, 
+        longitude decimal      
+    );
 """)
 
 time_table_create = ("""
+    CREATE TABLE IF NOT EXISTS time
+    (
+        start_time timestamp PRIMARY KEY, 
+        hour int,
+        day int,
+        week int,
+        month int,
+        year int,
+        weekday int
+    );
 """)
 
 # INSERT RECORDS
 
 songplay_table_insert = ("""
+    INSERT INTO songplays 
+    (
+        songplay_id, 
+        start_time, 
+        user_id, 
+        level, 
+        song_id, 
+        artist_id, 
+        session_id, 
+        location, 
+        user_agent
+    )
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);
 """)
 
 user_table_insert = ("""
+    INSERT INTO users 
+    (
+        user_id,
+        first_name, 
+        last_name, 
+        gender, 
+        level
+    )
+        VALUES (%s, %s, %s, %s, %s);
 """)
 
 song_table_insert = ("""
+    INSERT INTO songs 
+    (
+        song_id, 
+        title, 
+        artist_id, 
+        year, 
+        duration
+    )
+        VALUES (%s, %s, %s, %s, %s);
 """)
 
 artist_table_insert = ("""
+    INSERT INTO artists 
+    (
+        artist_id, 
+        name, 
+        location, 
+        lattitude, 
+        longitude
+    )
+        VALUES (%s, %s, %s, %s, %s);
 """)
 
-
 time_table_insert = ("""
+    INSERT INTO time 
+    (
+        start_time, 
+        hour, 
+        day, 
+        week, 
+        month, 
+        year, 
+        weekday
+    )
+        VALUES (%s, %s, %s, %s, %s, %s, %s);
 """)
 
 # FIND SONGS
