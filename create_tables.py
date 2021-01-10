9413fe278a6a65ea642db174ebb37603adf251d0
olivier felce <olivierfelce@hotmail.com>
2021-01-10 21:53:18 +0100

    Refactoring: Try and except added

--- create_tables.py
+++ create_tables.py
@@ -9,10 +9,14 @@ def create_database():
     """
     
     # connect to default database
-    conn = psycopg2.connect("host=127.0.0.1 dbname=studentdb user=student password=student")
-    conn.set_session(autocommit=True)
-    cur = conn.cursor()
-    
+    try:
+        conn = psycopg2.connect("host=127.0.0.1 dbname=studentdb user=student password=student")
+        conn.set_session(autocommit=True)
+        cur = conn.cursor()
+    except psycopg2.Error as e:
+        print("Error: Could not make connection to the Postgres database")
+        print(e) 
+
     # create sparkify database with UTF8 encoding
     cur.execute("DROP DATABASE IF EXISTS sparkifydb")
     cur.execute("CREATE DATABASE sparkifydb WITH ENCODING 'utf8' TEMPLATE template0")
